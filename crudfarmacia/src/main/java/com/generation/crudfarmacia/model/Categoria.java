5cf93248904448ac8874f66867296411d4ee118f
Rodrigo Sanchez <rodrigoschort01@gmail.com>
2024-01-04 12:46:50 -0300

    CRUD de Categoria adicionado

---
+++ crudfarmacia/src/main/java/com/generation/crudfarmacia/model/Categoria.java
@@ -0,0 +1,51 @@
+package com.generation.crudfarmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank(message = "O atributo nome é Obrigatório!")
+	@Size(min = 5, max = 60, message = "O atributo nome deve conter no mínimo 05 e no máximo 60 caracteres")
+	private String nome;
+
+	@NotBlank(message = "O atributo descricoa é Obrigatório!")
+	@Size(max = 150, message = "O atributo descricao deve conter no máximo 150 caracteres")
+	private String descricao;
+
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public void setNome(String titulo) {
+		this.nome = titulo;
+	}
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}
